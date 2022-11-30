2c9a7694539959c4f45b558c9904a79d81905efe
nesrindemir <nnessrrinn85@hotmail.com>
2022-11-30 16:00:21 +0300

    DEĞİŞTİRİLDİ

---
+++ ödevlerim/artikYilHesabi.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+public class artikYilHesabi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner input= new Scanner(System.in);
+      
+      int yil;
+      System.out.print("Bir yıl giriniz:");
+      yil=input.nextInt();
+      
+      int artikyilHesapla;
+      
+      artikyilHesapla=(yil %4);
+      
+  
+     
+       
+       if(artikyilHesapla==0) {
+    	   
+    	     
+    	   
+    	if(yil%100==0){
+    		
+    		if(yil%400==0) {
+    			
+    			  System.out.println(yil + " bir artık yıldır !");	
+    			
+    		}else {
+    		
+    		  System.out.println(yil + " bir artık yıl değildir !");
+    	   }
+        
+    	}	
+      
+       }else {
+      
+    	  
+    	  System.out.println(yil + " bir artık yıl değildir !");
+    	  
+       } 
+      
+	}
+
+}
