fc6b04e2a33d6511ef24b7f04dfd85c2b4c10a65
nesrindemir <nnessrrinn85@hotmail.com>
2022-12-02 09:11:55 +0300

    .

--- ödevlerim/src/TaksimetreUcretHesabı.java
+++ ödevlerim/src/TaksimetreUcretHesabı.java
@@ -1,19 +1,18 @@
-package odev_1;
 import java.util.Scanner;
-public class TaksimetreUcretHesabý {
+public class TaksimetreUcretHesabï¿½ {
 	public static void main(String[] args) {
       
 		 Scanner input= new Scanner(System.in);
 		 
-		double acilisUcret,artýsUcret,minUcret,toplamTutar;
+		double acilisUcret,artï¿½sUcret,minUcret,toplamTutar;
 		int gidilenMesafe;
 		
 		System.out.println("Gidilen mesafeyi giriniz :");
 		gidilenMesafe=input.nextInt();
 		
 		acilisUcret=10;
-		artýsUcret=2.20;
-		toplamTutar=acilisUcret+(gidilenMesafe*artýsUcret);
+		artï¿½sUcret=2.20;
+		toplamTutar=acilisUcret+(gidilenMesafe*artï¿½sUcret);
 		
 		if(toplamTutar<=20) {
 			
